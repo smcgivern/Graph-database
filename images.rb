f6e32a7877db69d792e1d9d290fa10342b3a3a31
Sean McGivern <sean@mcgivern.me.uk>
2009-07-19 13:43:44 +0100

    Initial commit

---
+++ images.rb
@@ -0,0 +1,11 @@
+SED = "sed -i -e 's/<svg.*/<svg width=\"100%\" height=\"100%\"/'"
+PNG = 'rsvg-convert -a -h 400'
+PNGS = 'rsvg-convert -a -h 25'
+
+Dir['dot/*.dot'].each do |file|
+  base = file.split('/').last.split('.').first
+  `neato -n -opublic/image/#{base}.svg -Tsvg #{file}`
+  `#{SED} public/image/#{base}.svg`
+  `#{PNG} -o public/image/#{base}.png public/image/#{base}.svg`
+  `#{PNGS} -o public/image/small/#{base}.png public/image/#{base}.svg`
+end
---
+++ text.rb
@@ -0,0 +1,40 @@
+require 'erb'
+
+def load(database)
+  graphs = []
+
+  open(database).each do |line|
+    name, data = line.gsub('(', '|[').gsub(')', ']').split('|')
+    data = eval(data)
+
+    nodes = {}
+    data[3].each_with_index {|c, i| nodes["#{i+1}"] = c.join(',')}
+
+    graphs << {
+      :name => name, :n => data[0], :m => data[1],
+      :degrees => data[2].map {|x, y| [x] * y}.flatten,
+      :nodes => nodes, :edges => data[4],
+    }
+  end
+
+  graphs
+end
+
+def create_template(filename); ERB.new(open(filename).read); end
+
+GRAPHS = load('database.gdb')
+GRAPHVIZ = create_template('graph.dot.erb')
+INDEX = create_template('index.html.erb')
+GRAPH_PAGE = create_template('graph.html.erb')
+
+open('public/index.html', 'w').puts(INDEX.result)
+
+GRAPHS.each do |graph|
+  g = graph
+
+  open("dot/#{g[:name]}.dot", 'w').
+    puts(GRAPHVIZ.result(binding))
+
+  open("public/#{g[:name]}.html", 'w').
+    puts(GRAPH_PAGE.result(binding))
+end
