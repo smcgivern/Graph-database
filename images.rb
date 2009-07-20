d5cdb4f5512343d77628175c905482a03724889b
Sean McGivern <sean@mcgivern.me.uk>
2009-07-20 20:17:47 +0100

    Added command-line option to only run for a particular graph

--- images.rb
+++ images.rb
@@ -1,8 +1,9 @@
 SED = "sed -i -e 's/<svg.*/<svg width=\"100%\" height=\"100%\"/'"
 PNG = 'rsvg-convert -a -h 400'
 PNGS = 'rsvg-convert -a -h 25'
+GLOB = ARGV[0] || '*'
 
-Dir['dot/*.dot'].each do |file|
+Dir["dot/#{GLOB}.dot"].each do |file|
   base = file.split('/').last.split('.').first
   `neato -n -opublic/image/#{base}.svg -Tsvg #{file}`
   `#{SED} public/image/#{base}.svg`
