d3b1d0e3b62b87d143bb45b13caded37e3df48a4
Sean McGivern <sean@mcgivern.me.uk>
2011-03-12 14:25:00 +0000

    Use CSS sprites for image thumbnails

--- images.rb
+++ images.rb
@@ -1,12 +1,5 @@
-SED = "sed -i -e 's/<svg.*/<svg width=\"100%\" height=\"100%\"/'"
-PNG = 'rsvg-convert -a -h 400'
-PNGS = 'rsvg-convert -a -h 25'
-GLOB = ARGV[0] || '*'
-
-Dir["dot/#{GLOB}.dot"].each do |file|
-  base = file.split('/').last.split('.').first
-  `neato -n -opublic/image/#{base}.svg -Tsvg #{file}`
-  `#{SED} public/image/#{base}.svg`
-  `#{PNG} -o public/image/#{base}.png public/image/#{base}.svg`
-  `#{PNGS} -o public/image/small/#{base}.png public/image/#{base}.svg`
+Dir['dot/*.dot'].each do |file|
+  `./create-images #{file.split('/').last.split('.').first} #{file}`
 end
+
+`./create-sprites`
